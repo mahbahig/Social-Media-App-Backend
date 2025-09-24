63d7a7385da620dae695711b0614ae0c83c3a6c2
mahbahig <mahbahig2@gmail.com>
2025-09-25 02:03:29 +0300

    Edited the user interface

--- src/shared/interfaces/user/user.interface.ts
+++ src/shared/interfaces/user/user.interface.ts
@@ -1,21 +1,21 @@
-import { Types } from "mongoose";
+import { ObjectId } from "mongoose";
 import { UserGender, UserProvider, UserRole } from "../../enums";
 
 export interface IUser {
-    _id: Types.ObjectId,
-    firstName: string,
-    lastName?: string,
-    username?: string,
-    email: string,
-    password: string,
-    age?: number,
-    gender?: UserGender,
-    profilePicture?: string,
-    role: UserRole,
-    provider: UserProvider,
-    isVerified: boolean,
-    createdAt: Date,
-    updatedAt: Date,
-    otp?: string,
-    otpExpiration?: Date
+    _id: ObjectId;
+    firstName: string;
+    lastName?: string;
+    username?: string;
+    email: string;
+    password: string;
+    age?: number;
+    gender?: UserGender;
+    profilePicture?: string;
+    role: UserRole;
+    provider: UserProvider;
+    confirmed: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    otp?: string;
+    otpExpiration?: Date;
 }
