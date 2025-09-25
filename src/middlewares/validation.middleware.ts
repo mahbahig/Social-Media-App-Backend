c57cf226b8feff41dd0c5446d04cf03b79240895
mahbahig <mahbahig2@gmail.com>
2025-09-26 02:03:33 +0300

    Refactored validation middleware

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -7,25 +7,20 @@ type SchemaType = Partial<Record<RequestType, ZodType>>;
 
 export const validation = (schema: SchemaType) => {
     return (req: Request, res: Response, next: NextFunction) => {
-        // Array to hold validation errors
-        let validationErrors: Object[] = [];
         // Validate each part of the request based on the provided schema
         for (const key of Object.keys(schema) as RequestType[]) {
             // Skip if no schema is provided for this part
             if (!schema[key]) continue;
             const result = schema[key].safeParse(req[key]);
+            // Check if validation failed
             if (!result.success) {
                 // Format and collect errors and add to the validationErrors array
-                const formattedErrors = result.error.issues.map((err) => ({
-                    path: err.path.join("."),
-                    message: err.message,
+                const formattedErrors = result.error.issues.map((issue) => ({
+                    path: issue.path[0],
+                    message: issue.message
                 }));
-                validationErrors.push(...formattedErrors);
+                throw new ValidationException("Validation error", formattedErrors);
             }
-        }
-        // If there are validation errors, respond with 400 and the errors
-        if (validationErrors.length) {
-            throw new ValidationException("Validation error", validationErrors);
         }
         next();
     };
