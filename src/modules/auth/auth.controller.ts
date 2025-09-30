40c06f08e249b45a8ce52a5322a6e7308fe73b23
mahbahig <mahbahig2@gmail.com>
2025-09-30 16:20:17 +0300

    Removed unused next parameter in the auth controller

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,7 +4,7 @@ import { ConfirmEmailDTO, RegisterDTO } from "./auth.dto";
 
 class AuthController {
     /********************************* Register *********************************/
-    register = async (req: Request, res: Response, next: NextFunction) => {
+    register = async (req: Request, res: Response) => {
         const registerDTO: RegisterDTO = req.body;
 
         const { accessToken, refreshToken } = await AuthService.register(registerDTO);
@@ -13,7 +13,7 @@ class AuthController {
     };
 
     /********************************* Confirm Email *********************************/
-    confirmEmail = async (req: Request, res: Response, next: NextFunction) => {
+    confirmEmail = async (req: Request, res: Response) => {
         const confirmEmailDTO: ConfirmEmailDTO = req.body;
 
         await AuthService.confirmEmail(confirmEmailDTO);
@@ -22,7 +22,7 @@ class AuthController {
     };
 
     /********************************* Login *********************************/
-    login = async (req: Request, res: Response, next: NextFunction) => {
+    login = async (req: Request, res: Response) => {
         const loginDTO = req.body;
 
         const { accessToken, refreshToken } = await AuthService.login(loginDTO);
