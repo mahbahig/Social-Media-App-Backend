510bbe01ec5fc07d3cb7313dcbcc54a0679e9a4b
mahbahig <mahbahig2@gmail.com>
2025-09-30 23:46:26 +0300

    Attached user object to Request interface from express

--- src/shared/interfaces/index.ts
+++ src/shared/interfaces/index.ts
@@ -1,1 +1,11 @@
+import { IUser } from "./user/user.interface";
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: IUser;
+        }
+    }
+}
+
 export * from "./user/index";
