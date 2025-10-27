fcdfe09d5de84047ac100757b5d3d7fd033b0123
mahbahig <mahbahig2@gmail.com>
2025-10-27 23:46:03 +0300

    Added get profile feature in the user module

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,3 +1,11 @@
-class UserController {}
+import { Request, Response } from "express";
+
+class UserController {
+    /********************************* Get Profile *********************************/
+    getProfile = async (req: Request, res: Response) => {
+        const user = req.user;
+        res.status(200).json({ user });
+    };
+}
 
 export default new UserController();
--- src/modules/user/user.router.ts
+++ src/modules/user/user.router.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import { isAuthenticated } from "../../middlewares";
+import UserController from "./user.controller";
 
 const userRouter = Router({ caseSensitive: true, strict: true });
+
+userRouter.get("/profile", isAuthenticated, UserController.getProfile);
 
 export default userRouter;
